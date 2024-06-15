39162778fa535f2481f67f2e14867493fb882966
Quan-business <phunghongquan.business@gmail.com>
2024-06-15 22:13:37 +0700

    shell script for installing stable version of docker for Ubuntu 20.04

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # installation_documentation
+This is repo provides the complete installation guide for applications, probably bug fix during installing process
+
+Current document include:
+`docker`: stable version(focal) for Ubuntu 20.04
+
---
+++ docker.sh
@@ -0,0 +1,41 @@
+# install old docker versions and packages
+$ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Update existing packages
+sudo apt update
+
+# install prerequisite
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+# Add GPG key for the official Docker repository to your system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the Docker repository to APT sources.
+# It automatically install the stable version of Docker meant for Ubuntu 20.04 which is 'focal' 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+apt-cache policy docker-ce
+
+# install docker without prompt and stuff
+# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# install docker
+yes | sudo apt install docker-ce docker-ce-cli containerd.io
+
+
+## Use docker command without sudo
+
+sudo groupadd docker
+
+# add your username to the docker group
+sudo usermod -aG docker ${USER}
+
+# To apply the new group membership
+newgrp docker
+
+# verify the connection to the Docker server
+docker info
+
+# verfiy the Docker engine
+docker run hello-world
+
