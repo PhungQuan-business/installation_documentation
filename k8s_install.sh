866ad01d05c1e9bcab0792f6b408b7174ee8d16a
Quan-business <phunghongquan.business@gmail.com>
2024-06-21 15:27:28 +0700

    add k8s installation guide

--- k8s_install.sh
+++ k8s_install.sh
@@ -1,19 +1,55 @@
-# Kubernetes components
-# update package
-sudo apt update
+# Kubernetes components v1.29
 
-# upgrade packages
-sudo apt upgrade
+# install theo k8s tools docs
+# https://v1-29.docs.kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-# add sigining key into the system(repeat on other node)
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-# add software repository, since k8s is not included in ubuntu repository by default
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update -y
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
-# install k8s tools
-sudo apt-get install -y kubelet kubeadm kubectl -y
+##### Từ đây trở xuống là phải viết thành docs##########
 
-sudo apt-mark hold kubelet kubeadm kubectl -y
+# change the default cgroup to systemd
+sudo mkdir -p /etc/docker
+sudo tee /docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+# enable cri(container runtime interface)
+sudo vi /etc/containerd/config.toml
+# comment the only line and save
+sudo systemctl restart containerd
+
+# intialize the cluster on master node
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=<MASTER_IP>
+
+# for the master node only
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# apply network plugin
+kubectl apply -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml
+
+# copy the join and run it on workers node
