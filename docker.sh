419b56026400a2ac13dbc91d130e72773537b75c
Quan-business <phunghongquan.business@gmail.com>
2024-06-28 10:28:28 +0700

    modify the installation steps for docker, using steps provided by docker website

--- docker.sh
+++ docker.sh
@@ -1,26 +1,21 @@
 # install old docker versions and packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Update existing packages
-sudo apt update
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# install prerequisite
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-# Add GPG key for the official Docker repository to your system
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Add the Docker repository to APT sources.
-# It automatically install the stable version of Docker meant for Ubuntu 20.04 which is 'focal' 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
-apt-cache policy docker-ce
-
-# install docker without prompt and stuff
-# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# install docker
-yes | sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 ## Use docker command without sudo
